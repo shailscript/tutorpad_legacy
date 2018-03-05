abae4a9a00b666f4ba1baeaca4e318deafe000fb
Shailendra Shukla <shailendrashuklapro@gmail.com>
2018-03-06 00:40:21 +0530

    Added UI for user home -for mock inspection of system

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,348 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <div class="container">
+          {{-- @foreach ($products->chunk(3) as $items) --}}
+              <div class="row">
+                  {{-- @foreach ($items as $product) --}}
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                    @endif
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  {{-- @endforeach --}}
+              </div>
+
+
+              <div class="row">
+                  {{-- @foreach ($items as $product) --}}
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  {{-- @endforeach --}}
+              </div>
+
+
+              <div class="row">
+                  {{-- @foreach ($items as $product) --}}
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img
+                            class="card-img-top"
+                            alt="Product image"
+                            style="width: 100%; display: block;"
+                            src=" " >
+
+                        <div class="card-body">
+                            <h5>Product One</h5>
+                            <p class="card-text">
+                                Awesome product.Awesome product.Awesome product.Blah blah
+                            </p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small>&#8377; 50</small>
+
+                                <div class="">
+                                    <button
+                                        type="button"
+                                        class="btn btn-sm btn-outline-secondary"
+                                        data-toggle="modal"
+                                        data-target="#modal-">
+                                        Quick look
+                                    </button>
+
+                                    <a
+                                        href=""
+                                        class="btn btn-sm btn-primary ml-1">
+                                        Buy
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                  {{-- @endforeach --}}
+              </div>
+          {{-- @endforeach --}}
+      </div>
 @endsection
