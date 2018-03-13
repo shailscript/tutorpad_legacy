2b6fc787b3cdcd088bb843d417ac3e9f788693e4
Shailendra Shukla <shailendrashuklapro@gmail.com>
2018-03-13 23:36:57 +0530

    Updated app config for demo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Product</title>
+    <title>TutorPad</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -81,7 +81,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                PRODUCT NAME
+                TutorPad
             </div>
         </div>
     </div>
