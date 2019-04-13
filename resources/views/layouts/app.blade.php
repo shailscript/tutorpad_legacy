29f39adefe858aef08b2767e7b83f84d49a177c8
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-12 23:37:45 -0400

    Chart.js demo removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
+  <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,23 +15,29 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
     <!-- Scripts -->
-<script src="{{ asset('js/app.js') }}"></script>
-</head>
-<body>
-<div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+    <script src="{{ asset('js/app.js') }}"></script>
+  </head>
+  <body>
+    <div id="app">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
         <div class="container">
           <a class="navbar-brand" href="{{ route('institute.index') }}">
             TutorPad
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+          </a>
+          <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                <ul class="navbar-nav">
-                    @if (Auth::guest())
+          <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              @if (Auth::guest())
                 <li class="nav-item">
                   <a class="nav-link" href="{{ route('login') }}">Student Login</a>
                 </li>
@@ -44,8 +50,8 @@
                 <li class="nav-item">
                   <a class="nav-link" href="{{ route('admin.register')}}">Register your Tuition</a>
                 </li>
-                    @else
-                        <li class="nav-item dropdown">
+              @else
+                <li class="nav-item dropdown">
                   <a
                   href="#"
                   class="nav-link dropdown-toggle"
@@ -53,10 +59,10 @@
                   data-toggle="dropdown"
                   aria-haspopup="true"
                   aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </a>
+                    {{ Auth::user()->name }}
+                  </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
                     @if ( Auth::guard('web')->check() )
                       <a href="{{ route('stats.boards') }}" class="dropdown-item">Board wise statistics</a>
                       <a href="{{ route('stats.ratings') }}" class="dropdown-item">Ratings wise statistics</a>
@@ -66,98 +72,57 @@
                     <a
                     href="{{ Auth::guard('admin')->check() ? route('admin.logout') : route('user.logout') }}"
                     class="dropdown-item"
-                                   onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
+                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                      Logout
+                    </a>
 
                     <form
                     id="logout-form"
                     action="{{ (Auth::guard('admin')->check()) ? route('admin.logout') : route('user.logout') }}"
                     method="POST"
-                                      style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </div>
-                        </li>
-                    @endif
-                </ul>
-            </div>
+                    style="display: none;">
+                      {{ csrf_field() }}
+                    </form>
+                  </div>
+                </li>
+              @endif
+            </ul>
+          </div>
 
         </div>
-    </nav>
+      </nav>
 
-    <div class="container">
+        <div class="container">
             @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-            @if(Session::has('alert-' . $msg))
-                <p class="alert alert-{{ $msg }}">
-                    {{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                </p>
-            @endif
+                @if(Session::has('alert-' . $msg))
+                    <p class="alert alert-{{ $msg }}">
+                        {{ Session::get('alert-' . $msg) }} <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    </p>
+                @endif
             @endforeach
+        </div>
+        @yield('content')
     </div>
-    @yield('content')
-</div>
 
-<br><br>
-<hr>
-<br>
+    <br><br>
+    <hr>
+    <br>
 
-<footer class="text-muted">
-    <div class="container">
+    <footer class="text-muted">
+      <div class="container">
 
-    <p class="float-right">
-        <a href="#">Back to top</a>
-    </p>
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
 
-    <p>TutorPad is a one stop solution for Tuition classes. TutorPad is awesome. Add some nice description.
+        <p>TutorPad is a one stop solution for Tuition classes. TutorPad is awesome. Add some nice description.
            TutorPad is awesome. Add some nice description. TutorPad is awesome. Add some nice description.
         </p>
-    </div>
-</footer>
+      </div>
+    </footer>
     <br>
   </body>
 
   @yield('scripts')
 
-</body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
-<script>
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-</script>
 </html>
