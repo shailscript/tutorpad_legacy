88c0b413c5c0eebea72993df0d82ee5b20c956b1
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-28 21:49:05 -0400

    Tutor dashboard view added

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,27 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"> <h1>Add your institute</h1> </div>
-                <br>
-                <form action=" ">
+  <div class="container">
+    <div class="row mb-5">
+      <div class="col-9">
+        <h1>All Institutes</h1>
+      </div>
+      <div class="col-3">
+        <a href="{{ route('institute.create') }}" class="btn btn-lg btn-block btn-dark">Add an institute</a>
+      </div>
 
-                    <div class="row form-group">
-                        <div class="col-sm-3"><label for="">Title</label></div>
-                        <div class="col-sm-7"><input type="text"></div>
-                    </div>
-
-                    <div class="row form-group">
-                        <div class="col-sm-3"><label for="">Description</label></div>
-                        <div class="col-sm-7"><input type="text"></div>
-                    </div>
-
-                </form>
-            </div>
-        </div>
     </div>
-</div>
+    <div class="row">
+      <table class="table">
+        <thead>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Board</th>
+          <th>location</th>
+          <th>Email</th>
+          <th>Action</th>
+        </thead>
+        <tbody>
+          @foreach ($admin->institutes as $institute )
+            <tr>
+              <td>{{ $institute->name }}</td>
+              <td>{{ $institute->tuition_type }}</td>
+              <td>{{ $institute->board_type }}</td>
+              <td>{{ $institute->location }}</td>
+              <td>{{ $institute->email }}</td>
+              <td>
+                <a href="{{ route('institute.show', $institute->id) }}" class="btn btn-info">View</a>
+                <a href="{{ route('institute.edit', $institute->id) }}" class="btn btn-warning">Edit</a>
+                <a href="{{ route('institute.destroy', $institute->id) }}" class="btn btn-danger">Delete</a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
 @endsection
