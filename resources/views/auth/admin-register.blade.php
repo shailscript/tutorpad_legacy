ffd8943b4a3c708b0418f995ed9f0742d717a4c2
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-04-28 21:44:27 -0400

    Tutor registration made simple

--- resources/views/auth/admin-register.blade.php
+++ resources/views/auth/admin-register.blade.php
@@ -24,21 +24,6 @@
                                 @endif
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="contact_number" class="col-md-4 col-form-label text-md-right">Contact Number</label>
-
-                            <div class="col-md-6">
-                                <input id="contact_number" type="text" class="form-control{{ $errors->has('contact_number') ? ' is-invalid' : '' }}" name="contact_number" value="{{ old('contact_number') }}" required autofocus>
-
-                                @if ($errors->has('contact_number'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('contact_number') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
 
@@ -52,94 +37,6 @@
                                 @endif
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="tuition_type" class="col-md-4 col-form-label text-md-right">Type of Tuition</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" name="tuition_type">
-                                  <option value="private">Private Tuitions</option>
-                                  <option value="group">Group Tuitions</option>
-                                </select>
-
-                                @if ($errors->has('tuition_type'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('tuition_type') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="class_from" class="col-md-4 col-form-label text-md-right">Class from</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" name="class_from">
-                                  <option value="1">1</option>
-                                  <option value="2">2</option>
-                                  <option value="3">3</option>
-                                  <option value="4">4</option>
-                                  <option value="5">5</option>
-                                  <option value="6">6</option>
-                                  <option value="7">7</option>
-                                  <option value="8">8</option>
-                                  <option value="9">9</option>
-                                  <option value="10">10</option>
-                                  <option value="11">11</option>
-                                  <option value="12">12</option>
-                                </select>
-
-                                @if ($errors->has('class_from'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('class_from') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-
-                            <label for="class_to" class="col-md-4 col-form-label text-md-right">Class to</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" name="class_to">
-                                  <option value="1">1</option>
-                                  <option value="2">2</option>
-                                  <option value="3">3</option>
-                                  <option value="4">4</option>
-                                  <option value="5">5</option>
-                                  <option value="6">6</option>
-                                  <option value="7">7</option>
-                                  <option value="8">8</option>
-                                  <option value="9">9</option>
-                                  <option value="10">10</option>
-                                  <option value="11">11</option>
-                                  <option value="12">12</option>
-                                </select>
-
-                                @if ($errors->has('class_to'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('class_to') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="board_type" class="col-md-4 col-form-label text-md-right">Board</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" name="board_type">
-                                  <option value="cbse">CBSE</option>
-                                  <option value="gseb">GSEB</option>
-                                  <option value="icse">ICSE</option>
-                                </select>
-
-                                @if ($errors->has('board_type '))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('board_type') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
 
