392ea8e2b502b0246017b812a3004fc07d4fc4ac
Shailendra Shukla <shailendrashuklapro@gmail.com>
2019-05-01 22:31:56 -0400

    Chart.js integration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,35 @@
 @section('content')
   <div class="container">
               <div class="row">
-                <canvas id="myChart" width="10" height="10"></canvas>
+                <canvas id="myChart"></canvas>
               </div>
       </div>
+@endsection
+
+@section('scripts')
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
+  <script>
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+          labels: ["CBSE", "ICSE", "GSEB"],
+          datasets: [{
+              label: '# of institutes',
+              data: [{{$data['cbse']}}, {{$data['icse']}}, {{$data['gseb']}}],
+              backgroundColor: [
+                  'rgba(13, 60, 85, 1)',
+                  'rgba(19, 149, 186, 1)',
+                  'rgba(241, 108, 32, 1)'
+             ],
+         }]
+
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true
+      }
+  });
+  </script>
+
 @endsection
